74bce0c13aa0d8032364e019f1770ff8589f73a2
Leandro <leandroalegregabriel@outlook.com>
2025-07-11 20:44:41 -0300

    database.js: se actualizó el nombre de la base de datos

--- rest-api/src/database.js
+++ rest-api/src/database.js
@@ -6,7 +6,7 @@ const properties = {
   host: 'localhost',  // La base está en mi compu (localhost)
   user: 'root',       // Usuario que uso para entrar a la base
   password: '',       // Como no le puse contraseña, queda vacío
-  database: 'alegre_leandro' // Nombre de la base de datos que uso
+  database: 'biblioteca' // Nombre de la base de datos que uso
 };
 
 // Creo un pool de conexiones para manejar mejor las consultas y exporto eso
