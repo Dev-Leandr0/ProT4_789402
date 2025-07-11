d357ae51d36818b3393e3d504c94f4f5856f0a72
Leandro <leandroalegregabriel@outlook.com>
2025-07-11 20:45:10 -0300

    routes.js: se agregó la ruta GET /libro para obtener un libro por ID

--- rest-api/src/routes.js
+++ rest-api/src/routes.js
@@ -1,16 +1,22 @@
 // Importo Router de express para armar las rutas del servidor
 import { Router } from 'express';
-// Traigo el controlador de usuario que tiene las funciones para manejar las peticiones
-import { usuario } from './controller.js';
+// Traigo el controlador de libro que tiene las funciones para manejar las peticiones
+import { libro } from './controller.js';
 
 // Creo el router para definir las rutas
 export const router = Router()
 
-// Ruta GET para traer todos los usuarios, llama a la función getAll del controlador
-router.get('/usuarios', usuario.getAll);
-// Ruta POST para agregar un usuario nuevo, llama a la función add del controlador
-router.post('/usuario', usuario.add);
-// Ruta POST para eliminar un usuario, llama a la función delete del controlador
-router.delete('/usuario', usuario.delete);
-// Ruta POST para actualizar un usuario, llama a la función update del controlador
-router.put('/usuario', usuario.update);
+// Ruta GET para traer todos los libros, llama a la función getAll del controlador
+router.get('/libros', libro.getAll);
+
+// Ruta GET para obtener un libro por ID
+router.get('/libro', libro.getOne);
+
+// Ruta POST para agregar un libro nuevo, llama a la función add del controlador
+router.post('/libro', libro.add);
+
+// Ruta POST para eliminar un libro, llama a la función delete del controlador
+router.delete('/libro', libro.delete);
+
+// Ruta POST para actualizar un libro, llama a la función update del controlador
+router.put('/libro', libro.update);
