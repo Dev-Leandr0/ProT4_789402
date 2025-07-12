6e223db63b1a7e574365c29e4f540c38d5c20471
Leandro <leandroalegregabriel@outlook.com>
2025-07-11 21:44:53 -0300

    Agrega script de datos de prueba para la base de datos

---
+++ rest-api/script/datos_prueba.sql
@@ -0,0 +1,6 @@
+INSERT INTO libros (nombre, autor, categoria, anio_publicacion, ISBN) VALUES
+('Cien años de soledad', 'Gabriel García Márquez', 'Novela', '1967-06-05', '9780307474728'),
+('El Principito', 'Antoine de Saint-Exupéry', 'Ficción', '1943-04-06', '9780156012195'),
+('Don Quijote de la Mancha', 'Miguel de Cervantes', 'Clásico', '1605-01-16', '9788491050279'),
+('1984', 'George Orwell', 'Distopía', '1949-06-08', '9780451524935'),
+('La sombra del viento', 'Carlos Ruiz Zafón', 'Misterio', '2001-04-01', '9788408051387');
